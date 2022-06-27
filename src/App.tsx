2fd5db1908f3ad2d023b4806c890466233f1998b
Daniil <daniilgavriluk765@gmail.com>
2022-06-27 18:30:03 +0300

    les4

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {TodoList,PropsArrayType} from "./components/TodoList";
+import {TodoList, PropsArrayType} from "./components/TodoList";
+import {v1} from 'uuid'
 
+export type FilterValuesType = "all" | "completed" | "active"
+const App = () => {
+    console.log(v1())
 
-const App=()=> {
-    const tasks:Array<PropsArrayType>=[
-        {id:1, title:"HTML&CSS", isDone:true},
-        {id:1, title:"JS", isDone:false},
-        {id:1, title:"ReactJS", isDone:false}
-    ]
+    const toToListTitle: string = "What to learn";
+
+    //данные для отрисовки
+    const [tasks, setTasks] = useState<Array<PropsArrayType>>([
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: false},
+            {id: v1(), title: "ReactJS", isDone: false}
+        ]
+    );
+    //добавление нового объекта в массив(добавление новой таски)
+    const addTask = (title: string) => {
+
+        const newTask: PropsArrayType = {
+            id: v1(), title: title, isDone: false
+        };
+
+        setTasks([newTask, ...tasks])
+    };
+    //удаление таски
+    const removeTask = (taskID: string) => {
+        let remove = tasks.filter(t => t.id !== taskID)
+        setTasks(remove)
+    };
+
+    //фильтрация тасок
+    const [filter, setFilter] = useState<FilterValuesType>("all");
+    const changeFilter = (filter: FilterValuesType) => {
+        setFilter(filter)
+    };
+    //управлением чекс боксом
+    const changeTaskStatus = (taskID:string, isDone:boolean) => {
+      setTasks(tasks.map(t=>t.id===taskID ? {...t, isDone: isDone} : t))
+    };
 
     return (
         <div className="App">
             <TodoList
-                title="What to learn"
+                title={toToListTitle}
                 tasks={tasks}
+                filter={filter}
+
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+                addTask={addTask}
+                changeTaskStatus={changeTaskStatus}
             />
         </div>
     );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,35 +1,117 @@
-import React from "react";
+import React, {ChangeEvent, useState, KeyboardEvent} from "react";
+import {FilterValuesType} from "../App";
 
-type PropsTitleType={
-    title:string
-    tasks:PropsArrayType[]
+type PropsType = {
+    title: string
+    tasks: PropsArrayType[]
+    filter: FilterValuesType
+
+    removeTask: (taskID: string) => void
+    changeFilter: (filter: FilterValuesType) => void
+    addTask: (title: string) => void
+    changeTaskStatus: (taskID: string, isDone: boolean) => void
 }
 
-export type PropsArrayType={
-    id:number
-    title:string
-    isDone:boolean
+export type PropsArrayType = {
+    id: string
+    title: string
+    isDone: boolean
 }
 
-export const TodoList:React.FC<PropsTitleType> = (props) => {
+export const TodoList: React.FC<PropsType> = (props) => {
+
+    const [title, setTitle] = useState<string>("")//локальный стейт для input
+    const [error, setError] = useState<boolean>(false)//локальный стейт для валидации ввода символов в input
+
+    const addTask = () => {
+        const taskTitle = title.trim() //trim обрезает пробелы
+        if (taskTitle) {// если таска без пробелов, то добавляем
+            props.addTask(taskTitle)
+        } else {
+            setError(true) //ошибка есть и начинает отрисовывать <div>Title is required!</div>
+        }
+        //очищение поля ввода после добавления таски
+        setTitle('')
+    };
+
+    const onChangeTask = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+        if (error) setError(false)// если мы допустили ошибку и дальше начинаем печатать, то ошибка снимается
+    };
+    const onKeyPressTask = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            addTask()
+        }
+    };
+    //стили
+    const allBtnClasses = props.filter === 'all' ? 'active-button' : '';
+    const activeBtnClasses = props.filter === 'active' ? 'active-button' : '';
+    const completedBtnClasses = props.filter === 'completed' ? 'active-button' : '';
+    const errorInputStyle = error ? {border: '2px solid red', outline: 'none'} : undefined;
+
+
+    const getTaskForRender = () => {
+        let taskRender = props.tasks
+        switch (props.filter) {
+            case 'active':
+                taskRender = props.tasks.filter(t => !t.isDone)
+                break
+            case 'completed':
+                taskRender = props.tasks.filter(t => t.isDone)
+                break
+        }
+        return taskRender
+    };
+
+    const taskRender = getTaskForRender()
+    const taskJSXElements = taskRender.length//проверка на пустой массив
+        ? taskRender.map((t) => {
+            const taskClasses = t.isDone ? 'is-done' : '' //стиль спана
+            return (
+                <li key={t.id}>
+                    <input type="checkbox" checked={t.isDone}
+                           onChange={(event) => props.changeTaskStatus(t.id, event.currentTarget.checked)}/>
+                    <span className={taskClasses}>{t.title}</span>
+                    <button onClick={() => props.removeTask(t.id)}>Deleted</button>
+                </li>
+            )
+        })
+        : <span>List is empty</span>;//если массив пустой, то выводит спан
 
     return (
         <div>
             <div>
                 <h3>{props.title}</h3>
                 <div>
-                    <input/>
-                    <button>+</button>
+                    <input
+                        style={errorInputStyle}
+                        //синхронизация
+                        value={title}
+                        //отслеживаем написанное в инпут
+                        onChange={onChangeTask}
+                        //добавление таски по нажатию Enter
+                        onKeyPress={onKeyPressTask}
+                    />
+                    <button onClick={addTask}>+</button>
+                    {/*//если error true то отрисовывает div*/}
+                    {error && <div style={{color: 'red', fontWeight: 'bold'}}>Title is required!</div>}
                 </div>
                 <ul>
-                    <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                    <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                    <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+                    {taskJSXElements}
                 </ul>
                 <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
+                    <button className={allBtnClasses} onClick={() => {
+                        props.changeFilter("all")
+                    }}>All
+                    </button>
+                    <button className={activeBtnClasses} onClick={() => {
+                        props.changeFilter("active")
+                    }}>Active
+                    </button>
+                    <button className={completedBtnClasses} onClick={() => {
+                        props.changeFilter("completed")
+                    }}>Completed
+                    </button>
                 </div>
             </div>
         </div>
